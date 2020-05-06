6cf3c623bdd1a6e977bb80c807e0f9892c8b5f81
Alex <RoxexXIII@gmail.com>
2020-05-06 14:45:07 +0200

    login & register
    with the error

--- sdi1920-entrega2-1920-310-1920-313/app.js
+++ sdi1920-entrega2-1920-310-1920-313/app.js
@@ -143,10 +143,6 @@ app.set('clave', 'solo_leveling');
 app.set('crypto', crypto);
 
 require('./routes/rusuarios.js')(app, swig, gestorBD);
-require('./routes/rcanciones.js')(app, swig, gestorBD);
-require('./routes/rautores.js')(app, swig);
-require('./routes/rcomentarios.js')(app, swig, gestorBD);
-require("./routes/rapicanciones.js")(app, gestorBD);
 
 app.get('/', function(req, res) {
     res.redirect('/tienda');
